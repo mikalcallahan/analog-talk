a16290f4c4b599c62da4710deec0d562faa1c841
Mikal Callahan <mikal.callahan@icloud.com>
2024-04-15 17:05:03 -0400

    main - PaymentsPage init

---
+++ src/app/pages/payments.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payments',
+  template: `<div>Payments page</div>`,
+  standalone: true,
+})
+export default class PaymentsPage {}
