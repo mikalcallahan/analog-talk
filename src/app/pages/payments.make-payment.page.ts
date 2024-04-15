c50424790d2d02d086f70265300b08cc6545c2b1
Mikal Callahan <mikal.callahan@icloud.com>
2024-04-15 18:01:39 -0400

    main - MakePaymentPage

---
+++ src/app/pages/payments.make-payment.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-make-payments',
+  template: `<div>Make Payment page</div>`,
+  standalone: true,
+})
+export default class MakePaymentsPage {}
