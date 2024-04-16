3d5a48dcd1cd7e41e8472c47deba81b1db2c16fd
Mikal Callahan <mikal.callahan@icloud.com>
2024-04-15 21:18:05 -0400

    main - feat: LoginPage & SignupPage

---
+++ src/app/pages/(auth)/login.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  template: '<div>login here</div>',
+  standalone: true,
+})
+export default class LoginPage {}
---
+++ src/app/pages/(auth)/signup.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  template: '<div>signup here</div>',
+  standalone: true,
+})
+export default class SignupPage {}
