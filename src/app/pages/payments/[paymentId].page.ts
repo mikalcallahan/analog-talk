a6f3e1ce2af191d522e3e02a8f3a3dbfd4b7c517
Mikal Callahan <mikal.callahan@icloud.com>
2024-04-16 15:12:52 -0400

    main - refactor: product dynamic route with input

--- src/app/pages/payments/[paymentId].page.ts
+++ src/app/pages/payments/[paymentId].page.ts
@@ -1,21 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { Component, Input } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [AsyncPipe],
   template: `
     <h2>Payment Details</h2>
 
-    ID: {{ paymentId$ | async }}
+    ID: {{ paymentId }}
   `,
 })
 export default class PaymentDetailsPage {
-  private readonly route = inject(ActivatedRoute);
-
-  readonly paymentId$ = this.route.paramMap.pipe(
-    map((params) => params.get('paymentId')),
-  );
+  @Input() paymentId: string | null = null;
 }
