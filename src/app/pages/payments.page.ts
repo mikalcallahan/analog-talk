0f6c30bf5b2efd2803c5e3a8d069ed7f00a3a0b1
Mikal Callahan <mikal.callahan@icloud.com>
2024-04-15 20:28:17 -0400

    main - payments nested route

--- src/app/pages/payments.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-payments',
-  template: `<div>Payments page</div>`,
-  standalone: true,
-})
-export default class PaymentsPage {}
--- src/app/pages/payments/make-payment.page.ts
+++ src/app/pages/payments/make-payment.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-make-payments',
+  selector: 'app-make-payment',
   template: `<div>Make Payment page</div>`,
   standalone: true,
 })
-export default class MakePaymentsPage {}
+export default class MakePaymentPage {}
