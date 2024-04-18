320369226f2c09010eac28debed46da80e582930
Mikal Callahan <mikal.callahan@icloud.com>
2024-04-17 22:13:38 -0400

    main - feat: prerender root, /about, and /contact

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,13 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [
+    analog({
+      prerender: {
+        routes: ['/', '/about', '/contact'],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
